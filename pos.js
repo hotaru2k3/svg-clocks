64b61c6277ae53201d5e433558dc7f0d6ca21aa1
lily wilson <hotaru@thinkindifferent.net>
2014-12-07 20:22:35 -0500

    improve initial timezone-based geolocation, and move geolocation code to a separate file

---
+++ pos.js
@@ -0,0 +1,11 @@
+(function()
+{ var d = new Date();
+  var jan = new Date(d.getFullYear(), 0, 1).getTimezoneOffset();
+  var jul = new Date(d.getFullYear(), 6, 1).getTimezoneOffset();
+
+  window.pos = { latitude: jul < jan ? 45 : jan > jul ? -45 : 0,
+                 longitude: -Math.max(jan, jul) / 4 };
+  if(window.google && google.loader && google.loader.clientLocation)
+    pos = google.loader.clientLocation;
+  if(navigator.geolocation)
+    navigator.geolocation.watchPosition(function(p) { pos = p.coords; }); })();
